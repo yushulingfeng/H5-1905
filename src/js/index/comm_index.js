3ef177c8fec84fe4be094819e090be8c73bbc51f
玉树凌风 <1396852379@qq.com>
2019-07-08 16:59:21 +0800

    yes

--- src/js/index/comm_index.js
+++ src/js/index/comm_index.js
@@ -14,15 +14,39 @@
     //美妆专区
     $('.Beauty_makeupFloor').load('html/page/index/Beauty_makeupFloor.html');
 
-
+    //吸顶搜索栏
     window.onscroll = function() {
-        if (scrollY >= 100) {
+        if (scrollY >= 50) {
             $('.topFixed').css('display', 'block');
         } else {
             $('.topFixed').css('display', 'none');
         }
     }
 
+    //回到顶部悬挂侧栏
+    let suspension = document.querySelector('.suspension_right');
+    let floor_jump = document.querySelector('.floor_jump');
 
+    document.onscroll = function() {
+        // console.log(scrollY)
+        // console.log(suspension.offsetHeight)
+        // let top = window.innerHeight - suspension.offsetHeight;
+        if (scrollY >= 655) {
+            suspension.style.position = 'fixed';
+            suspension.style.top = '70px';
+            floor_jump.style.position = 'fixed';
+            floor_jump.style.top = '70px';
+        } else {
+            suspension.style.position = 'absolute';
+            suspension.style.top = '55px';
+            floor_jump.style.position = 'absolute';
+            floor_jump.style.top = '55px';
+        }
+    }
+
+    //点击回到顶部事件
+    $('.suspension_right').on('click', '.topBtn', function() {
+        window.scrollTo(0, 0);
+    })
 
 })();
