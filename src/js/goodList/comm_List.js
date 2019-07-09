eba79b94985fe06bcf52d7295a2a40053970fdf3
玉树凌风 <1396852379@qq.com>
2019-07-09 15:17:46 +0800

    yes

---
+++ src/js/goodList/comm_List.js
@@ -0,0 +1,10 @@
+(() => {
+
+    $('header').load('http://www.kl.com/src/html/component/header.html');
+
+    $('#navBox').load('http://www.kl.com/src/html/component/nav.html')
+
+    $('footer').load('http://www.kl.com/src/html/component/footer.html');
+
+
+})();
